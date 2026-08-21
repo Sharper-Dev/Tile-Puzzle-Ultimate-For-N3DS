437ca09ea0a2a8a03dfb7927496bd8f48384bc7f
Sharper Dev <guilherme.senadeaguiar626@gmail.com>
2026-08-20 22:44:10 -0300

    Replace local lerp with MathE.lerp in circle scripts

--- romfs/assets/scripts/objects/bottom_circles.lua
+++ romfs/assets/scripts/objects/bottom_circles.lua
@@ -1,6 +1,7 @@
 local GameObject = require("gameobject.m2d_gameobject")
 local Debugger = require("debugger.m2d_debugger")
 local Time = require("time.m2d_time")
+local MathE = require("extender.math.m2d_math")
 
 local thisObject = GameObject:new("bottom_circles")
 local Script = thisObject:addComponent("Script", {})
@@ -12,9 +13,6 @@ local finalPos = { x = 190, y = 86 }
 local speed = 0.5
 local counter = 0
 
-local function lerp(a, b, t)
-    return a + (b - a) * t
-end
 
 function Script:start()
     thisObject.transform:setPosition(startPos.x, startPos.y, 1)
@@ -25,8 +23,8 @@ end
 function Script:update()
     counter = counter + Time.deltaTime * speed
     
-    local x = lerp(startPos.x, finalPos.x, math.min(counter, 1.0))
-    local y = lerp(startPos.y, finalPos.y, math.min(counter, 1.0))
+    local x = MathE.lerp(startPos.x, finalPos.x, math.min(counter, 1.0))
+    local y = MathE.lerp(startPos.y, finalPos.y, math.min(counter, 1.0))
     
     thisObject.transform:setPosition(x, y)
     if counter >= 1.0 then
