c81b7060c8fc788d11a6a842ae4f145ce51c87b2
Sharper Dev <guilherme.senadeaguiar626@gmail.com>
2026-09-17 21:14:18 -0300

    Move romfs assets into src and update build scripts
    Rename/move project assets and micro2d sources from romfs/... to src/... (preserves directory structure). Update tools/3ds_buildscript.bat, tools/3dsx_buildscript.bat and tools/cia_buildscript.bat to use --romfs-dir "../src" when creating romfs.bin for builds. No other code logic changes.

--- tools/3dsx_buildscript.bat
+++ tools/3dsx_buildscript.bat
@@ -1,2 +1,2 @@
-3dstool -cvtf romfs "../romfs.bin" --romfs-dir "../romfs"
+3dstool -cvtf romfs "../romfs.bin" --romfs-dir "../src"
 3dsxtool "../elf/lpp-3ds.elf" "../build/lpp-3ds.3dsx" --romfs="../romfs.bin" --smdh="../build_assets/lpp-3ds.smdh"
