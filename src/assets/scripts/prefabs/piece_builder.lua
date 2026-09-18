bd838f5e67baed126948ab84146b37d6d6adfc7b
Sharper Dev <guilherme.senadeaguiar626@gmail.com>
2026-09-17 23:07:50 -0300

    Fix docs, add drag movement to piece

--- src/assets/scripts/prefabs/piece_builder.lua
+++ src/assets/scripts/prefabs/piece_builder.lua
@@ -1,6 +1,7 @@
 local PieceBuilder = {}
 local GameObject = require("gameobject.m2d_gameobject")
 local Debugger = require("debugger.m2d_debugger")
+local InputSystem = require("systems.input.m2d_input_system")
 
 function PieceBuilder.createPiece(name, spritePath)
     local pieceObject = GameObject:new(name)
@@ -15,11 +16,25 @@ function PieceBuilder.createPiece(name, spritePath)
     local boxCollider = pieceObject:addComponent("BoxCollider")
     boxCollider:setSize(62, 62)
     boxCollider:setOffset(-30, -30)
+    
     boxCollider.onTouchDown = function(self)
         Debugger.msg("down! I am " .. self.gameObject.name)
+        self.dragging = true
+        local pieceLayer = self.gameObject.transform.position.z
+        self.gameObject.transform:setPosition(nil, nil, pieceLayer + 1)
     end
+
+    boxCollider.onTouchStay = function(self)
+        if not self.dragging then return end
+        local x, y = InputSystem.getTouch()
+
+        self.gameObject.transform:setPosition(x, y)
+    end
+
     boxCollider.onTouchUp = function(self)
-        Debugger.msg("up! I am " .. self.gameObject.name)
+        self.dragging = false
+        local pieceLayer = self.gameObject.transform.position.z
+        self.gameObject.transform:setPosition(nil, nil, pieceLayer - 1)
     end
     return pieceObject
 end
